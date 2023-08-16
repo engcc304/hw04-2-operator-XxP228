4c8aa4b2f2136bfae9fe85226f8a67a0aae23f7a
XxP228 <kamochnok.2548@gmail.com>
2023-08-16 22:21:58 +0700

    hw4-2

--- Homework/HW04/hw04-2-operator.cpp
+++ Homework/HW04/hw04-2-operator.cpp
@@ -1,6 +1,3 @@
-/*
-    จงเติมโค้ดโปรแกรมตามที่แสดงในส่วนของ Placeholder เพื่อแสดงผลลัพธ์ของ Operator ด้านซ้ายมือ พร้อมทั้งแก้ไขโค้ดโปรแกรมให้ถูกต้อง
-*/
 #include <stdio.h>
 int main() {
     int a = 0, b = 0 ;
@@ -12,7 +9,7 @@ int main() {
     printf( "a - b = %d \n", a - b ) ;
     printf( "a * b = %d \n", a * b ) ;
     printf( "a / b = %d (b must greater than 0)\n", b > 0 ? a : 0 ) ;
-    printf( "a % b = %d \n", a % b ) ;
+    printf( "a %% b = %d \n", a % b ) ;
     printf( "++a = %d \n", ++a ) ;
     printf( "a-- = %d \n", a-- ) ;
     printf( "a += 1 %d \n", a += 1 ) ;
@@ -21,8 +18,8 @@ int main() {
     printf( "a -= b %d \n", a -= b ) ;
     printf( "a *= 1 %d \n", a *= 1 ) ;
     printf( "a *= b %d \n", a *= b ) ;
-    printf( "a %= 1 %d \n", a %= 1 ) ;
-    printf( "a %= b %d \n", a %= b ) ;
+    printf( "a %%= 1 %d \n", a %= 1 ) ;
+    printf( "a %%= b %d \n", a %= b ) ;
     printf( "a && b %d \n", a && b ) ;
     printf( "a || b %d \n", a || b ) ;
     return 0 ;
